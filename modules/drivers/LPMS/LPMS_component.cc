3270125570917baa6c8b9e296164cc7facdec3b0
Bowie <zbw0377@163.com>
2020-08-03 10:30:44 +0800

    drivers: modify BUILD

--- modules/drivers/LPMS/LPMS_component.cc
+++ modules/drivers/LPMS/LPMS_component.cc
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *****************************************************************************/
-#include "modules/drivers/LPMS/driver/LPMS_component.h"
+#include "modules/drivers/LPMS/LPMS_component.h"
 
 
 namespace apollo
