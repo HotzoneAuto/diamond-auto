fcc3f4b66abd1d86b21faeb36d7f2ccb12c02d79
zongbao <mickeyouou@gmail.com>
2020-08-07 18:25:32 +0800

    Control: build pass onboard

--- modules/control/demo_control_component.h
+++ modules/control/demo_control_component.h
@@ -4,26 +4,33 @@
 #include <string>
 
 #include "cyber/class_loader/class_loader.h"
-#include "cyber/component/timer_component.h"
+#include "cyber/component/component.h"
 
 #include "modules/canbus/proto/chassis.pb.h"
 #include "modules/control/proto/control_cmd.pb.h"
-#include "modules/common/global_gflags.h"
+#include "modules/common/adapters/adapter_gflags.h"
 
 
 namespace apollo {
 namespace control {
-using apollo::cyber::Component;
 
-class ControlComponent: public Component<>{
+using apollo::cyber::Component;
+using apollo::canbus::Chassis;
+using apollo::control::ControlCommand;
+
+class ControlComponent: public Component<> {
 public:
-  ControlComponent();
-  bool ControlComponent::Init() override;
-  void ControlComponent::GenerateCommand();
+  // ControlComponent();
+  bool Init() override;
+  void GenerateCommand();
   ~ControlComponent();
 
+private:
   Chassis chassis_;
-  
+  std::shared_ptr<cyber::Reader<apollo::canbus::Chassis>> chassis_reader_;
+  std::shared_ptr<cyber::Writer<ControlCommand>> control_cmd_writer_;
+
+  std::future<void> async_action_;
 };
 
 CYBER_REGISTER_COMPONENT(ControlComponent)
