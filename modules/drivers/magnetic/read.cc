fd02c5dafb3315bdc02701de970b9d5bcf65bb7d
zongbao <mickeyouou@gmail.com>
2020-08-13 16:29:25 +0800

    magetic: build pass with lat dev

--- modules/drivers/magnetic/read.cc
+++ modules/drivers/magnetic/read.cc
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <iostream>
 
 #include "cyber/common/log.h"
@@ -14,16 +15,14 @@ typedef unsigned char BYTE;
 using namespace std;
 using apollo::drivers::Magnetic;
 
-char Hex2Ascii(char hex) {
-  AINFO << "hex to transfer :" << hex;
-  char c = toupper(hex);
-
-  if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F')) {
-    BYTE t = (c >= 'A') ? c - 'A' + 10 : c - '0';
-
-    return t << 4;
+int decToBin(int dec) {
+  int result = 0, temp = dec, j = 1;
+  while (temp) {
+    result = result + j * (temp % 2);
+    temp = temp / 2;
+    j = j * 10;
   }
-  return 'N';
+  return result;
 }
 
 // JUST DATA MODE
@@ -38,11 +37,10 @@ void OnData(std::shared_ptr<apollo::cyber::Node> node) {
   std::shared_ptr<apollo::cyber::Writer<apollo::drivers::Magnetic>>
       magnetic_writer_ =
           node->CreateWriter<apollo::drivers::Magnetic>(FLAGS_magnetic_channel);
+
   while (!apollo::cyber::IsShutdown()) {
     // Send read Data message
-    // unsigned char msg_cmd[] = {0x01, 0x03, 0x00, 0x01, 0x00,
-                              //  0x01, 0xd5, 0xca, 0};
-    // TODO(wangying): remove untill test okay
+    // char msg_read_cmd = {0x01, 0x03, 0x00, 0x01, 0x00, 0x01, 0xd5, 0xca};
     char msg_read_cmd[8];
     msg_read_cmd[0] = 0x01;
     msg_read_cmd[1] = 0x03;
@@ -52,11 +50,11 @@ void OnData(std::shared_ptr<apollo::cyber::Node> node) {
     msg_read_cmd[5] = 0x01;
     msg_read_cmd[6] = 0xd5;
     msg_read_cmd[7] = 0xca;
-    int result = device_.Write(msg_read_cmd, 9);
+    int result = device_.Write(msg_read_cmd, 8);
     ADEBUG << "Magnetic Msg Read Cmd Send result is :" << result;
 
     count = 1;
-    std::memset(buffer, 0, 10);  
+    std::memset(buffer, 0, 10);
     while (1) {
       int ret = device_.Read(&buf, 1);
       if (ret == 1) {
@@ -74,10 +72,46 @@ void OnData(std::shared_ptr<apollo::cyber::Node> node) {
       header->set_timestamp_sec(apollo::cyber::Time::Now().ToSecond());
       header->set_frame_id("magnetic");
 
-      AINFO << "RETURN ID buffer[3] : " << static_cast<int>(buffer[3]);
-      AINFO << "RETURN ID buffer[4] : " << static_cast<int>(buffer[4]);
+      AINFO << "RETURN ID buffer[3] : "
+            << static_cast<int>(buffer[3]);  // low 8
+      AINFO << "RETURN ID buffer[4] : "
+            << static_cast<int>(buffer[4]);  // high 8
 
-      // magnetic.set_id(static_cast<int>(buffer[10]));
+      string s_l = to_string(decToBin(buffer[4]));
+      string s_h = to_string(decToBin(buffer[3]));
+
+      while (s_l.size() < 8) {
+        s_l = '0' + s_l;
+      }
+
+      while (s_h.size() < 8) {
+        s_h = '0' + s_h;
+      }
+
+      char ss[16] = {0};
+      for (int i = 0; i < 16;
+           i++)  // convert 0000 0001 0000 0000 to 0000 0000 1000 0000, make
+                 // string each char in ss denote 1~16 in sequence.
+      {
+        if (i < 8) {
+          ss[i] = s_l[7 - i];
+        } else
+          ss[i] = s_h[15 - i];
+      }
+
+      int sum_activated = 0;
+      int sum_id = 0;
+      for (int i = 0; i < 16; i++) {
+        if (ss[i] == '1') {
+          sum_id += i + 1;
+          sum_activated += 1;
+        }
+      }
+
+      double lat_dev_mgs = 0;
+      lat_dev_mgs = double(sum_id) / double(sum_activated) - 8.5;
+
+      magnetic.set_lat_dev(static_cast<float>(lat_dev_mgs));
 
       magnetic_writer_->Write(magnetic);
     }
