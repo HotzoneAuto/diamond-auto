1027773e9e4c661d80fc8a1c1affcf36ca88c5e6
Bowie <zbw0377@163.com>
2020-08-17 17:29:04 +0800

    control: update magnetic control

--- modules/control/demo_control_component.cc
+++ modules/control/demo_control_component.cc
@@ -1,55 +1,128 @@
 #include "modules/control/demo_control_component.h"
+#include "modules/control/diamond_control_pid.h"
 
 #include <string>
 #include "cyber/cyber.h"
 #include "cyber/time/rate.h"
 #include "math.h"
 
-namespace apollo {
-namespace control {
+namespace apollo
+{
+namespace control
+{
 
 using apollo::cyber::Rate;
 using apollo::cyber::Time;
 
-bool ControlComponent::Init() {
-  // Reader
-  chassis_reader_ = node_->CreateReader<Chassis>(
-      FLAGS_chassis_topic, [this](const std::shared_ptr<Chassis>& chassis) {
-        chassis_.CopyFrom(*chassis);
-      });
+bool ControlComponent::Init()
+{
+	// Reader
+	chassis_reader_ = node_->CreateReader<Chassis>(
+	                      FLAGS_chassis_topic, [this](const std::shared_ptr<Chassis>& chassis)
+	{
+		chassis_.CopyFrom(*chassis);
+	});
 
-  // create Writer
-  control_cmd_writer_ = node_->CreateWriter<ControlCommand>(FLAGS_control_command_topic);
+	// create Writer
+	control_cmd_writer_ = node_->CreateWriter<ControlCommand>(FLAGS_control_command_topic);
 
-  // compute control message in aysnc
-  async_action_ = cyber::Async(&ControlComponent::GenerateCommand, this);
+	// compute control message in aysnc
+	async_action_ = cyber::Async(&ControlComponent::GenerateCommand, this);
 
-  return true;
+	return true;
 }
 
 // write to channel
-void ControlComponent::GenerateCommand() {
-  auto cmd = std::make_shared<ControlCommand>();
+void ControlComponent::GenerateCommand()
+{
+	auto cmd = std::make_shared<ControlCommand>();
 
-  // frequency, TODO: reset
-  Rate rate(20.0);
+	// frequency, TODO: reset
+	Rate rate(20.0);
+	
+	// 初始化前后磁导航检测到的偏差值
+	float front_lat_dev_mgs = 0;  // TODO: lateral_dev_mgs need to changed according to MGS module.
+	float back_lat_dev_mgs = 0;
+	
+	int find_rfid_des = 0; //记录是否检测到终点的rfid
+	int dir = 0; //记录行驶方向, 0代表从A到B, 1代表从B到A
+	
+	// TODO：发送扭矩，PID天闯
+	int motor_flag = 0; //控制驱动电机正反转
+	float motor_speed = 0; //驱动电机转速
+	float motor_torque = 0; //驱动电机转矩
+	
+	int front_steering_dir = 0; //控制前方转向电机正反转
+	int back_steering_dir = 0; //控制后方转向电机正反转
+	
+	float front_steering_speed = 0; //rpm，前方转向电机转速
+	float back_steering_speed = 0; //rpm，后方转向电机转速
+	
+	cmd -> set_front_steering_target(0);
+	cmd -> set_back_steering_target(0); //初始时前后转向电机转角为0
+	
+	while (true)
+	{
+		if (find_rfid_des == 1) //检测到终点rfid
+			motor_torque = pid_speed(0); //PID控制目标是驱动电机停转
+			// 以0为车速目标，向canbus发送经过PID后的转矩
+		else if (find_rfid_des == 0) //未检测到终点rfid
+			motor_torque = pid_speed(1); //驱动电机驱动汽车以1m/s运动，这是PID目标，尽可能接近1
+			// 以1为车速目标，向canbus发送经过PID后的转矩
+			
+		// 遥控，人工给输入
+		if (dir == 0) //若行驶方向从A到B（前进）
+			motor_flag = 0;
+		else if (dir == 1) //若行驶方向从B到A（后退）
+			motor_flag = 1;
 
-  while (true) {
-    float lat_dev_mgs =
-        5;  // TODO: lateral_dev_mgs need to changed according to MGS module.
-    if (lat_dev_mgs < -4.5 || lat_dev_mgs > 4.5) {
-      cmd->set_steering_target(10);
-    } else {
-      cmd->set_steering_target(0);
-    }
-    control_cmd_writer_->Write(cmd);
-    rate.Sleep();
-  }
+		if (/* motor_speed == 1 && */ motor_flag == 0)  // 若驱动电机正转（前进）
+		{
+			back_steering_dir = 2; //后方转向电机不转
+			if (front_lat_dev_mgs < -4.5)  //若前方磁导航检测出车偏左
+			{
+				front_steering_dir = 0; //则前方转向电机正转（即向右）
+				cmd -> set_front_steering_target(10); //转向量为10，待标定
+			}
+			else if (frong_lat_dev_mgs > 4.5)  //若前方磁导航检测出车偏右
+			{
+				front_steering_dir = 1; //则前方转向电机反转（即向左）
+				cmd -> set_front_steering_target(10); //转向量为10，待标定
+			}
+			else
+				front_steering_dir = 2; //前方转向电机不转
+		}
+		else if (/* motor_speed == 1*/ motor_flag == 1)  // 若驱动电机反转（倒车）
+		{
+			front_steering_dir = 2; //前方转向电机不转
+			if (back_lat_dev_mgs < -4.5)  //若后方磁导航检测出车偏左
+			{
+				back_steering_dir = 0; //则后方转向电机正转（即向右）
+				cmd -> set_back_steering_target(10); //转向量为10
+			}
+			else if (frong_lat_dev_mgs > 4.5)  //若后方磁导航检测出车偏右
+			{
+				back_steering_dir = 1; //则后方转向电机反转（即向左）
+				cmd -> set_back_steering_target(10); //转向量为10
+			}
+			else
+				back_steering_dir = 2; //后方转向电机不转
+		}
+		else{// 若出现异常
+			motor_speed = 0;
+			front_steering_dir = 2;
+			back_steering_dir = 2;
+		}
+
+		control_cmd_writer_->Write(cmd);
+		rate.Sleep();
+	}
 }
 
-ControlComponent::~ControlComponent() {
-  // back chassis handle
-  async_action_.wait();
+ControlComponent::~ControlComponent()
+{
+	// back chassis handle
+	async_action_.wait();
 }
 
 }  // namespace control
---
+++ modules/control/diamondauto_control_pid.h
@@ -0,0 +1,241 @@
+#ifndef _DIAMONDAUTO_CONTROL_H_
+#define _DIAMONDAUTO_CONTROL_H_
+
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+static float kp_steer = 0.1;
+static float ki_steer = 0;
+static float kd_steer = 0.1;
+static float delta_u_steer;
+static float u_pre_steer = 0;
+static float u_steer;
+static int steer_direction;
+
+static float kp_speed = 0;
+static float ki_speed = 0;
+static float kd_speed = 0;
+static float delta_u_torque;
+static float u_pre_torque = 0;
+static float u_torque;
+
+static float steer_motor_spd;
+static float tire_steer_spd;
+static float steer_angle=0;
+static float veh_spd = 0;
+static float desired_v = 2; 
+
+static float speed_motor_deadzone = 0;
+
+static float pid_integral = 0; // 纵向pid累积量
+static float pid_error = 0; 
+static float pid_error_pre = 0;
+static float fmottq = 0;
+static float canbus_veh_spd = 0;
+static int veh_mode = 0;
+
+// void pid_steering(float & e2, float & e1, float & e); // 转向PID控制，PID输出控制量（转速）给转向电机
+// float steer_motor(float error); // 经过电机响应、车辆运动，输出下一时刻的磁导航偏差
+// void pid_speed(float desire_v); // 纵向速度PID控制，PID输出控制量（转矩）给驱动电机
+
+void Init()
+{
+	cout<<"初始化......"<<endl<<endl;
+};
+
+void Self_check()
+{
+	cout<<"车辆状态自检......"<<endl<<endl;
+};
+
+
+/*void pid_steering(float & e2, float & e1, float & e)
+{
+	cout<<"控制前的磁导航偏差："<<e2<<"、"<<e1<<"、"<<e<<endl;
+
+	delta_u_steer = kp_steer*(e-e1)+ki_steer*(e)+kd_steer*(e-2*e1+e2); // pid输出的控制量增量
+	
+	u_steer = u_pre_steer + delta_u_steer;
+
+	cout<<"PID输出给转向电机的控制量："<<u_steer<<endl;
+
+	u_pre_steer = u_steer;
+
+	// 判断电机正反转
+	if (e>0)
+	{
+		cout<<"转向轮应左转"<<endl;
+		steer_direction = 1; 
+	}
+	else if (e<0)
+	{
+		cout<<"转向轮应右转"<<endl;
+		steer_direction = -1;
+	}
+	else
+	{
+		cout<<"转向轮应直行"<<endl;
+		steer_direction = 0;
+	}
+
+	e2 = e1;
+	e1 = e;
+	e = steer_motor(e);
+	
+	cout<<"经过PID调节，新的磁导航偏差序列："<<e2<<"、"<<e1<<"、"<<e<<endl;
+			
+};
+*/
+
+/* float steer_motor(float e)
+{
+	if (steer_direction == 1) // 左转
+	{
+		cout<<"电机左转"<<endl;
+		// CAN通讯给转向电机左转命令；		
+	}
+	else if(steer_direction == -1) // 右转
+	{
+		cout<<"电机右转"<<endl;
+		// CAN通讯给转向电机右转命令；		
+	}
+	
+
+	// 电机模型：由 pid 控制量 到 转向电机输出转速；
+	steer_motor_spd = u_steer;// 随便写的
+
+
+	// 减速机构：转向电机输出转速 通过减速机构 到 轮胎转向速度；
+	tire_steer_spd = steer_motor_spd/20;
+	
+
+	// 运动更新：轮胎转向速度 到 轮胎转角(角位移传感器)；车辆横纵向运动，状态更新
+	steer_angle = steer_angle + tire_steer_spd;//角位移传感器测量
+	
+
+	// 新的偏差检测：磁导航传感器检测当前偏差
+	cout<<"轮胎转角："<<steer_angle<<endl;
+		
+	float error_new;
+	error_new = e+sin(steer_angle*3.1415926/180)*veh_spd*100*0.05; //0.05s控制一次
+	// cout<<"新的偏差："<<error_new<<endl;
+	return error_new;
+};
+*/
+
+void simple_steering(float & e)
+{
+	if (e>4.5 || e<-4.5)
+	{
+		// 下发让转向电机开始转的信号
+		cout<<"转向电机开始转"<<endl;
+	}
+	else 
+	{
+		// 下发让转向电机停止转的信号	
+		cout<<"转向电机停止转"<<endl;
+	}
+
+}
+
+
+void rule_steering(float & e)
+{
+	// 判断前面转向电机正反转
+	if (e>4.5)
+	{
+		cout<<"转向轮应左转"<<endl; //根据偏差模块反馈的值进行调整
+		steer_direction = 1; 
+	}
+	else if (e<-4.5)
+	{
+		cout<<"转向轮应右转"<<endl;
+		steer_direction = 2;
+	}
+	else
+	{
+		cout<<"转向轮应直行"<<endl;
+		steer_direction = 0;
+	}
+
+	// 把正反转信号发给CAN通讯
+
+	// 前面转向电机接收CAN给的正反转信号
+
+	if (steer_direction == 1) // 左转
+	{
+		cout<<"电机左转"<<endl;
+		// CAN通讯给转向电机左转命令；	
+
+		if (e>=6)
+		{
+			steer_motor_spd = 1435; // 数据具体给多少需要算
+		}
+		else if (e>=3)
+		{
+			steer_motor_spd = 1435;	
+		}
+		else
+		{
+			steer_motor_spd = 1435;	
+		}
+	}
+	else if(steer_direction == 2) // 右转
+	{
+		cout<<"电机右转"<<endl;
+		// CAN通讯给转向电机右转命令；	
+
+		if (e<=-6)
+		{
+			steer_motor_spd = 1435;	// 转向电机额定转速1435rpm，目前不可控，正在尝试可控中，额定转速下是70s转1圈，车轮0.857rpm
+		}
+		else if (e<=-3)
+		{
+			steer_motor_spd = 1435;	
+		}
+		else
+		{
+			steer_motor_spd = 1435;	
+		}
+	}
+
+	cout<<"转速电机转速为"<<steer_motor_spd<<endl;
+	
+	e = e/2; // 此处应该是磁导航传感器实时接收的新偏差
+
+	cout<<"新的磁导航偏差为："<<e<<endl;
+}
+
+float pid_speed(float desire_v) // 目的是用pid求出转矩
+{
+	cout<<"pid中的期望速度："<<desire_v<<endl;
+	pid_error = desire_v - veh_spd;  // pid输入为当前车速误差
+	cout<< "纵向速度偏差："<<pid_error<<endl;
+
+	pid_integral += pid_error;
+	
+	// speed_motor_error应该是一个与期望速度相关的前馈量，给定期望速度可以算出一个该速度匀速下的目标转矩值
+	// speed_motor_error应该是整车目标转矩
+	u_torque = speed_motor_deadzone + kp_speed*pid_error + ki_speed*pid_integral + kd_speed*(pid_error-pid_error_pre);
+
+	cout<<"PID输出给驱动电机的控制量(转矩)："<<u_torque<<endl;
+
+	veh_spd = canbus_veh_spd; // 车速更新；需要根据给驱动电机的控制量计算出车速响应；	
+	
+	cout<<"当前车速："<<veh_spd<<endl;
+	pid_error_pre  = pid_error;	 
+	
+	return u_torque;
+	
+	// 将u_torque通过CAN通讯发送给驱动电机
+
+	// u_pre_torque = u_torque;
+
+	// 将CANbus反馈的车速赋给当前车速
+}
+
+
+#endif
