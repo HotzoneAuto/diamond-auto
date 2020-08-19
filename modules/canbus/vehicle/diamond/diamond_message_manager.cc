dbbdabb89eccfd12754e81b62278d76b0f7783b3
zongbao <mickeyouou@gmail.com>
2020-08-19 16:50:05 +0800

    canbus: add wheel angle can message support

--- modules/canbus/vehicle/diamond/diamond_message_manager.cc
+++ modules/canbus/vehicle/diamond/diamond_message_manager.cc
@@ -16,6 +16,8 @@
 
 #include "modules/canbus/vehicle/diamond/diamond_message_manager.h"
 
+#include "modules/canbus/vehicle/diamond/protocol/id_0x01.h"
+#include "modules/canbus/vehicle/diamond/protocol/id_0x02.h"
 #include "modules/canbus/vehicle/diamond/protocol/id_0x0c079aa7.h"
 #include "modules/canbus/vehicle/diamond/protocol/id_0x0c19f0a7.h"
 #include "modules/canbus/vehicle/diamond/protocol/id_0x0cfff3a7.h"
@@ -51,6 +53,8 @@ DiamondMessageManager::DiamondMessageManager() {
   AddRecvProtocolData<Id0x0c08a7f0, true>();
   AddRecvProtocolData<Id0x0c09a7f0, true>();
   AddRecvProtocolData<Id0x0c0ba7f0, true>();
+  AddRecvProtocolData<Id0x01, true>();
+  AddRecvProtocolData<Id0x02, true>();
   // CAN 2
   AddRecvProtocolData<Id0x1818d0f3, true>();
   AddRecvProtocolData<Id0x1819d0f3, true>();
