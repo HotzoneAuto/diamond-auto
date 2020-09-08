198fb7f4074c05919e73d1078d85524346257b5c
zongbao <mickeyouou@gmail.com>
2020-09-08 18:39:24 +0800

    add pad messages terminal

--- modules/control/control_component.cc
+++ modules/control/control_component.cc
@@ -34,10 +34,17 @@ bool ControlComponent::Init() {
       FLAGS_rfid_topic,
       [this](const std::shared_ptr<RFID>& rfid) { rfid_.CopyFrom(*rfid); });
 
-  // rfid Reader
+  // Padmsg Reader
   pad_msg_reader_ = node_->CreateReader<PadMessage>(
       FLAGS_pad_topic, [this](const std::shared_ptr<PadMessage>& pad_msg) {
         pad_msg_.CopyFrom(*pad_msg);
+        if (pad_msg != nullptr) {
+          ADEBUG << "pad_msg: " << pad_msg_.ShortDebugString();
+          if (pad_msg_.action() == DrivingAction::RESET) {
+            AINFO << "Control received RESET action!";
+          }
+          pad_received_ = true;
+        }
       });
 
   AINFO << "Control default driving action is "
@@ -62,9 +69,8 @@ double ControlComponent::PidSpeed(double veh_spd, double spd_motor_deadzone) {
   AINFO << "pid_e:" << pid_e << " pid_int:" << pid_int;
   auto pid_conf = control_conf_.pid_conf();
   double torque = 0;
-  AINFO << "pid_e_pre: " << pid_e_pre;
-  torque = 20.0 + pid_conf.kp() * pid_e + pid_conf.ki() * pid_int +
-           pid_conf.kd() * (pid_e - pid_e_pre);
+  torque = control_conf_.torque_deadzone() + pid_conf.kp() * pid_e +
+           pid_conf.ki() * pid_int + pid_conf.kd() * (pid_e - pid_e_pre);
 
   AINFO << "PID Output Torque：" << torque
         << " vehicle speed now:" << chassis_.speed_mps();
@@ -77,12 +83,8 @@ double ControlComponent::PidSpeed(double veh_spd, double spd_motor_deadzone) {
 
 // write to channel
 void ControlComponent::GenerateCommand() {
-  auto cmd = std::make_shared<ControlCommand>();
-
   // frequency
   Rate rate(100.0);
-
-  // static int MANUAL_BRAKE_SIGNAL = 0;
 
   float front_lat_dev_mgs = 0.0;
   float rear_lat_dev_mgs = 0.0;
@@ -92,111 +94,15 @@ void ControlComponent::GenerateCommand() {
 
   // 初始化驱动电机死区
   speed_motor_deadzone = control_conf_.torque_deadzone();
-  /*
-  if (veh_spd <= 0.1) {
-    // TODO: 落地后标定值
-    speed_motor_deadzone = control_conf_.torque_deadzone();
-  } else {
-    // 使用滚动阻力系数，此时死区指的是理论电机需求转矩
-    speed_motor_deadzone = r_wheel * m_veh * g * f_c / (i_1 * i_0 * yita_t);
-  }
-*/
-  // TODO:
-  // 突然断电停车后，需记录最后时刻的状态数据，包括前后轮转角；重新启动后再读取数据文件
-
-  // TODO: 在while判断前需要知道轮胎初始偏移角度，暂时先给0。
-  // front_wheel_angle_realtime = 0;
-  // rear_wheel_angle_realtime = 0;
   front_wheel_angle_realtime = chassis_.front_wheel_angle();
   rear_wheel_angle_realtime = chassis_.rear_wheel_angle();
 
-  //上过高压自检完成之后，进入自动驾驶模式后，车辆处于ready状态时
-  // while (front_wheel_angle_realtime > 0.5)  // 待标定
-  // {
-  // front_motor_steering_dir = 2;
-  // cmd->set_front_wheel_target(-10.0);
-
-  // 获取编码器瞬时角度值
-  // front_encoder_angle_realtime = chassis_.front_encoder_angle();
-
-  // front_wheel_angle_realtime = chassis_.front_wheel_angle();
-
-  /*
-  front_wheel_angle_realtime = update_wheel_angle(
-      front_wheel_angle_previous, front_encoder_angle_previous,
-      front_encoder_angle_realtime, encoder2wheel_gear_ratio);
-
-  front_wheel_angle_previous = front_wheel_angle_realtime;
-  front_encoder_angle_previous = front_encoder_angle_realtime;
-  */
-  // }
-
-  // while (rear_wheel_angle_realtime > 0.5)  // 待标定
-  // {
-  // rear_motor_steering_dir = 2;  //则后方转向电机反转（即向左）
-  // cmd->set_rear_wheel_target(-10.0);
-
-  // 获取编码器瞬时角度值
-  // rear_encoder_angle_realtime = chassis_.rear_encoder_angle();
-
-  // rear_wheel_angle_realtime = chassis_.rear_wheel_angle();
-
-  /*
-  rear_wheel_angle_realtime = update_wheel_angle(
-      rear_wheel_angle_previous, rear_encoder_angle_previous,
-      rear_encoder_angle_realtime, encoder2wheel_gear_ratio);
-
-  rear_wheel_angle_previous = rear_wheel_angle_realtime;
-  rear_encoder_angle_previous = rear_encoder_angle_realtime;
-  */
-  // }
-
-  // while (front_wheel_angle_realtime < -0.5)  // 待标定
-  // {
-  // front_motor_steering_dir = 1;  //则前方转向电机正转（即向右）
-  // cmd->set_front_wheel_target(10.0);
-
-  // 获取编码器瞬时角度值
-  // front_encoder_angle_realtime = chassis_.front_encoder_angle();
-
-  // front_wheel_angle_realtime = chassis_.front_wheel_angle();
-
-  /*
-  front_wheel_angle_realtime = update_wheel_angle(
-      front_wheel_angle_previous, front_encoder_angle_previous,
-      front_encoder_angle_realtime, encoder2wheel_gear_ratio);
-
-  front_wheel_angle_previous = front_wheel_angle_realtime;
-  front_encoder_angle_previous = front_encoder_angle_realtime;
-  */
-  // }
-
-  // while (rear_wheel_angle_realtime < -0.5)  // 待标定
-  // {
-  // rear_motor_steering_dir = 1;  //则后方转向电机正转（即向右）
-  // cmd->set_rear_wheel_target(10.0);
-
-  // 获取编码器瞬时角度值
-  // rear_encoder_angle_realtime = chassis_.rear_encoder_angle();
-
-  // rear_wheel_angle_realtime = chassis_.rear_wheel_angle();
-
-  /*
-  rear_wheel_angle_realtime = update_wheel_angle(
-      rear_wheel_angle_previous, rear_encoder_angle_previous,
-      rear_encoder_angle_realtime, encoder2wheel_gear_ratio);
-
-  rear_wheel_angle_previous = rear_wheel_angle_realtime;
-  rear_encoder_angle_previous = rear_encoder_angle_realtime;
-  */
-  // }
-
-  // cmd->set_front_wheel_target(0.0);
-
-  // cmd->set_rear_wheel_target(0.0);
-
   while (true) {
-    cmd->mutable_pad_msg()->CopyFrom(pad_msg_);
+    auto cmd = std::make_shared<ControlCommand>();
+    if (pad_received_) {
+      cmd->mutable_pad_msg()->CopyFrom(pad_msg_);
+      pad_received_ = false;
+    }
     // TODO: Configuration
     // 手动给定，0代表停止，1代表从A到B，2代表从B到A
     drivemotor_flag = 1;
@@ -236,51 +142,22 @@ void ControlComponent::GenerateCommand() {
       // 停止状态
       case 0: {
         cmd->set_torque(0);
-
         // TODO：检测到车速为0，驻车系统断气刹，车辆驻车停止
         break;
       }
     }
 
-    /*
-    // 遥控，人工给输入
-    if (veh_dir == 0) //若行驶方向从A到B（前进，A是第2辆车处，B是机械臂处）
-            drivemotor_flag = 1;
-    else if (veh_dir == 1) //若行驶方向从B到A（后退）
-            drivemotor_flag = 2;
-    */
-
-    /*
-    front_wheel_angle_realtime = update_wheel_angle(
-        front_wheel_angle_previous, front_encoder_angle_previous,
-        front_encoder_angle_realtime, encoder2wheel_gear_ratio);
-    rear_wheel_angle_realtime = update_wheel_angle(
-        rear_wheel_angle_previous, rear_encoder_angle_previous,
-        rear_encoder_angle_realtime, encoder2wheel_gear_ratio);
-    */
-
     front_wheel_angle_realtime = chassis_.front_wheel_angle();
     rear_wheel_angle_realtime = chassis_.rear_wheel_angle();
 
     // 检测到轮胎转角超过30°，转向电机停转
-    if (abs(front_wheel_angle_realtime) > 30) {
+    if (std::abs(front_wheel_angle_realtime) > 30) {
       // front_motor_steering_dir = 0;
     }
 
-    if (abs(rear_wheel_angle_realtime) > 30) {
+    if (std::abs(rear_wheel_angle_realtime) > 30) {
       // rear_motor_steering_dir = 0;
     }
-
-    /*
-    // TODO：收到人工介入制动信号进行制动
-    // TODO: manual_brake_signal:定义新通道专门用于自动控制下的人工交互
-    MANUAL_BRAKE_SIGNAL = chassis_.manual_brake_signal();
-    if ( MANUAL_BRAKE_SIGNAL != 0 ){
-      // TODO: 制动转矩，需改成标定值
-      drivemotor_torque = 50;
-      cmd->set_brake(drivemotor_torque);
-    }
-    */
 
     switch (FLAGS_magnetic_enable) {
       case 1: {
@@ -362,27 +239,6 @@ void ControlComponent::GenerateCommand() {
       }
       default: {}
     }
-
-    /*
-    // 更新轮胎转角和编码器度数
-    front_wheel_angle_realtime = update_wheel_angle(
-        front_wheel_angle_previous, front_encoder_angle_previous,
-        front_encoder_angle_realtime, encoder2wheel_gear_ratio);
-    rear_wheel_angle_realtime = update_wheel_angle(
-        rear_wheel_angle_previous, rear_encoder_angle_previous,
-        rear_encoder_angle_realtime, encoder2wheel_gear_ratio);
-
-    front_wheel_angle_previous = front_wheel_angle_realtime;
-    rear_wheel_angle_previous = rear_wheel_angle_realtime;
-
-    front_encoder_angle_previous = front_encoder_angle_realtime;
-    rear_encoder_angle_previous = rear_encoder_angle_realtime;
-
-    chassis_wheel_angle->set_front_wheel_angle(front_wheel_angle_realtime);
-    chassis_wheel_angle->set_rear_wheel_angle(rear_wheel_angle_realtime);
-
-    chassis_writer_->Write(chassis_wheel_angle);
-    */
 
     control_cmd_writer_->Write(cmd);
 
