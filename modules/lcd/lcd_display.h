c87d4138b39d1316fa482e7880ed741d0a743181
tao8687 <tao8786@outlook.com>
2020-09-09 19:49:42 +0800

    Lcd:lcd_display

---
+++ modules/lcd/lcd_display.h
@@ -0,0 +1,15 @@
+#include <memory>
+#include "cyber/cyber.h"
+#include "modules/lcd/proto/lcd.pb.h"
+
+
+namespace apollo {
+namespace drivers {
+namespace lcd {
+using apollo::cyber::Component;
+using apollo::cyber::Writer;
+using apollo::drivers::lcd;
+
+}
+}
+}
