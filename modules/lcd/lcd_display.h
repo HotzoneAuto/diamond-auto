9da3706ba7f0d3faae1fc6b3ab4c4c31a8868979
tao8687 <45028297+tao8687@users.noreply.github.com>
2020-09-11 00:39:12 +0800

    Debug0909 (#43)
    * canbus: destory 485 resources with destruction
    
    * rear frequency const speed
    
    * control: fixed control module shutdown
    
    * add rear frequency const speed
    
    * Control:debug
    
    * teleop: update torque with key t
    
    * control: destnation
    
    * canbus: magnetic message send by async thread
    
    * canbus: update magnetic thread
    
    * pid config:v=0.6,kp=35,ki=0.008,kd=0,deadzone=18
    
    * canbus & drivers: remove magnetic message to drivers dir
    
    * Lcd:lcd_display
    
    * all: control timer component
    
    * canbus: send battery set up can message by sync once
    
    * canbus global update
    
    * canbus: set motor voltage up by canbus init and stop
    
    * canbus: remove unused can messages
    
    * canbus: fixed teleop coredump
    
    * fix bug of magnetic lat_dev
    
    * canbus: high voltage async up
    
    * Lcd:lcd_display
    
    * Lcd:lcd_display update
    
    Co-authored-by: zongbao <mickeyouou@gmail.com>
    Co-authored-by: MengTC <mtc19@mails.tsinghua.edu.cn>
    Co-authored-by: tao8687 <tao8786@outlook.com>

--- modules/lcd/lcd_display.h
+++ modules/lcd/lcd_display.h
@@ -1,15 +1,39 @@
+#pragma once
+#include <cmath>
 #include <memory>
 #include "cyber/cyber.h"
-#include "modules/lcd/proto/lcd.pb.h"
+#include "modules/common/util/uart.h"
 
+#include "cyber/class_loader/class_loader.h"
+#include "cyber/component/component.h"
 
+#include "modules/canbus/proto/chassis.pb.h"
 namespace apollo {
 namespace drivers {
 namespace lcd {
-using apollo::cyber::Component;
-using apollo::cyber::Writer;
-using apollo::drivers::lcd;
 
-}
-}
-}
+using apollo::cyber::Component;
+
+using apollo::cyber::Writer;
+
+using apollo::canbus::Chassis;
+
+class LcdComponet : public apollo::cyber::Component<> {
+ public:
+  bool Init() override;
+  bool Proc();
+
+ private:
+  Chassis chassis_;
+  //  // TODO(all): auto config by udev
+  std::unique_ptr<Uart> device_ = nullptr;
+
+  std::future<bool> async_action_;
+
+  std::shared_ptr<cyber::Reader<apollo::canbus::Chassis>> chassis_reader_;
+};
+CYBER_REGISTER_COMPONENT(LcdComponet)
+}  // namespace lcd
+}  // namespace drivers
+}  // namespace apollo
+
