44b3b4491a67af75f8fe6fb289d29b360c7e66a8
zongbao <mickeyouou@gmail.com>
2020-09-11 12:47:29 +0800

    drivers: imu

---
+++ modules/drivers/imu/example.cc
@@ -0,0 +1,264 @@
+//===========================================================================//
+//
+// Copyright (C) 2020 LP-Research Inc.
+//
+// This file is part of OpenZen, under the MIT License.
+// See https://bitbucket.org/lpresearch/openzen/src/master/LICENSE for details
+// SPDX-License-Identifier: MIT
+//
+//===========================================================================//
+
+#include "OpenZen.h"
+
+#include <array>
+#include <atomic>
+#include <condition_variable>
+#include <iostream>
+#include <limits>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include <gsl/span>
+
+std::vector<ZenSensorDesc> g_discoveredSensors;
+std::condition_variable g_discoverCv;
+std::mutex g_discoverMutex;
+std::atomic_bool g_terminate(false);
+
+std::atomic<uintptr_t> g_imuHandle;
+std::atomic<uintptr_t> g_gnssHandle;
+
+using namespace zen;
+
+namespace {
+void addDiscoveredSensor(const ZenEventData_SensorFound& desc) {
+  std::lock_guard<std::mutex> lock(g_discoverMutex);
+  g_discoveredSensors.push_back(desc);
+}
+}  // namespace
+
+void pollLoop(std::reference_wrapper<ZenClient> client) {
+  while (!g_terminate) {
+    unsigned int i = 0;
+    while (true) {
+      const auto pair = client.get().waitForNextEvent();
+      const bool success = pair.first;
+      auto& event = pair.second;
+      if (!success) break;
+
+      if (!event.component.handle) {
+        switch (event.eventType) {
+          case ZenSensorEvent_SensorFound:
+            addDiscoveredSensor(event.data.sensorFound);
+            break;
+
+          case ZenSensorEvent_SensorListingProgress:
+            if (event.data.sensorListingProgress.progress == 1.0f)
+              g_discoverCv.notify_one();
+            break;
+        }
+      } else if ((g_imuHandle > 0) && (event.component.handle == g_imuHandle)) {
+        switch (event.eventType) {
+          case ZenImuEvent_Sample:
+            if (i++ % 100 == 0) {
+              std::cout << "Event type: " << event.eventType << std::endl;
+              std::cout << "> Event component: "
+                        << uint32_t(event.component.handle) << std::endl;
+              std::cout << "> Acceleration: \t x = " << event.data.imuData.a[0]
+                        << "\t y = " << event.data.imuData.a[1]
+                        << "\t z = " << event.data.imuData.a[2] << std::endl;
+              std::cout << "> Gyro: \t\t x = " << event.data.imuData.g[0]
+                        << "\t y = " << event.data.imuData.g[1]
+                        << "\t z = " << event.data.imuData.g[2] << std::endl;
+            }
+            break;
+        }
+      } else if ((g_gnssHandle > 0) &&
+                 (event.component.handle == g_gnssHandle)) {
+        switch (event.eventType) {
+          case ZenGnssEvent_Sample:
+            std::cout << "Event type: " << event.eventType << std::endl;
+            std::cout << "> Event component: "
+                      << uint32_t(event.component.handle) << std::endl;
+            std::cout << "> GPS Fix: \t = " << event.data.gnssData.fixType
+                      << std::endl;
+            std::cout << "> Longitude: \t = " << event.data.gnssData.longitude
+                      << "   Latitude: \t = " << event.data.gnssData.latitude
+                      << std::endl;
+            std::cout << " > GPS Time " << int(event.data.gnssData.year) << "/"
+                      << int(event.data.gnssData.month) << "/"
+                      << int(event.data.gnssData.day) << " "
+                      << int(event.data.gnssData.hour) << ":"
+                      << int(event.data.gnssData.minute) << ":"
+                      << int(event.data.gnssData.second) << " UTC" << std::endl;
+
+            if (event.data.gnssData.carrierPhaseSolution ==
+                ZenGnssFixCarrierPhaseSolution_None) {
+              std::cout << " > RTK not used" << std::endl;
+            } else if (event.data.gnssData.carrierPhaseSolution ==
+                       ZenGnssFixCarrierPhaseSolution_FloatAmbiguities) {
+              std::cout << " > RTK used with float ambiguities" << std::endl;
+            } else if (event.data.gnssData.carrierPhaseSolution ==
+                       ZenGnssFixCarrierPhaseSolution_FixedAmbiguities) {
+              std::cout << " > RTK used with fixed ambiguities" << std::endl;
+            }
+
+            break;
+        }
+      }
+    }
+  }
+
+  std::cout << "--- Exit polling thread ---" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+  ZenSetLogLevel(ZenLogLevel_Info);
+
+  g_imuHandle = 0;
+  g_gnssHandle = 0;
+
+  auto clientPair = make_client();
+  auto& clientError = clientPair.first;
+  auto& client = clientPair.second;
+
+  if (clientError) return clientError;
+
+  std::thread pollingThread(&pollLoop, std::ref(client));
+
+  std::cout << "Listing sensors:" << std::endl;
+
+  if (auto error = client.listSensorsAsync()) {
+    g_terminate = true;
+    client.close();
+    pollingThread.join();
+    return error;
+  }
+
+  std::unique_lock<std::mutex> lock(g_discoverMutex);
+  g_discoverCv.wait(lock);
+
+  if (g_discoveredSensors.empty()) {
+    g_terminate = true;
+    client.close();
+    pollingThread.join();
+    return ZenError_Unknown;
+  }
+
+  for (unsigned idx = 0; idx < g_discoveredSensors.size(); ++idx)
+    std::cout << idx << ": " << g_discoveredSensors[idx].name << " ("
+              << g_discoveredSensors[idx].ioType << ")" << std::endl;
+
+  unsigned int idx;
+  do {
+    std::cout << "Provide an index within the range 0-"
+              << g_discoveredSensors.size() - 1 << ":" << std::endl;
+    std::cin >> idx;
+  } while (idx >= g_discoveredSensors.size());
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+  auto sensorPair = client.obtainSensor(g_discoveredSensors[idx]);
+  auto& obtainError = sensorPair.first;
+  auto& sensor = sensorPair.second;
+  if (obtainError) {
+    g_terminate = true;
+    client.close();
+    pollingThread.join();
+    return obtainError;
+  }
+
+  auto imuPair = sensor.getAnyComponentOfType(g_zenSensorType_Imu);
+  auto& hasImu = imuPair.first;
+  auto imu = imuPair.second;
+
+  if (!hasImu) {
+    g_terminate = true;
+    client.close();
+    pollingThread.join();
+    return ZenError_WrongSensorType;
+  }
+
+  // store the handle to the IMU to identify data coming from the imu
+  // in our data processing thread
+  g_imuHandle = imu.component().handle;
+
+  // Get a string property
+  auto sensorModelPair =
+      sensor.getStringProperty(ZenSensorProperty_SensorModel);
+  auto& sensorModelError = sensorModelPair.first;
+  auto& sensorModelName = sensorModelPair.second;
+  if (sensorModelError) {
+    g_terminate = true;
+    client.close();
+    pollingThread.join();
+    return sensorModelError;
+  }
+  std::cout << "Sensor Model: " << sensorModelName << std::endl;
+
+  // enable this to stream the sensor data to a network address
+  // the ZEN_NETWORK build option needs to be enabled for this feature
+  // to work.
+  // sensor.publishEvents("tcp://*:8877");
+
+  // check if a Gnss component is present on this sensor
+  auto gnssPair = sensor.getAnyComponentOfType(g_zenSensorType_Gnss);
+  auto& hasGnss = gnssPair.first;
+  auto gnss = gnssPair.second;
+
+  if (hasGnss) {
+    // store the handle to the Gnss to identify data coming from the Gnss
+    // in our data processing thread
+    g_gnssHandle = gnss.component().handle;
+    std::cout << "Gnss Component present on sensor" << std::endl;
+
+    // enable this code for RTK forwarding from network
+    /*
+    if (gnss.forwardRtkCorrections("RTCM3Network", "192.168.1.117", 9000) !=
+    ZenError_None) { std::cout << "Cannot set RTK correction forwarding" <<
+    std::endl;
+    }
+    else {
+        std::cout << "RTK correction forwarding started" << std::endl;
+    }
+    */
+
+    // enable this code for RTK forwarding from serial port
+    /*
+    if (gnss.forwardRtkCorrections("RTCM3Serial", "COM11", 57600) !=
+    ZenError_None) { std::cout << "Cannot set RTK correction forwarding" <<
+    std::endl;
+    }
+    else {
+        std::cout << "RTK correction forwarding started" << std::endl;
+    }
+    */
+  }
+
+  // Enable sensor streaming
+  if (auto error = imu.setBoolProperty(ZenImuProperty_StreamData, true)) {
+    g_terminate = true;
+    client.close();
+    pollingThread.join();
+    return error;
+  }
+
+  std::string line;
+  while (!g_terminate) {
+    std::cout << "Type: " << std::endl;
+    std::cout << " - 'q' to quit;" << std::endl;
+    std::cout << " - 'r' to manually release the sensor;" << std::endl;
+
+    if (std::getline(std::cin, line)) {
+      if (line == "q")
+        g_terminate = true;
+      else if (line == "r")
+        sensor.release();
+    }
+  }
+
+  client.close();
+  pollingThread.join();
+  return 0;
+}
---
+++ modules/drivers/imu/imu_component.cc
@@ -0,0 +1,127 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+#include "modules/drivers/imu/imu_component.h"
+
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "modules/common/util/message_util.h"
+
+namespace apollo {
+namespace drivers {
+namespace imu {
+
+bool LPMSDriverComponent::Init() {
+  if (!GetProtoConfig(&device_conf_)) {
+    return false;
+  }
+  AINFO << "LPMS config: " << device_conf_.DebugString();
+
+  // enable resonable log output for OpenZen
+  ZenSetLogLevel(ZenLogLevel_Info);
+
+  // create OpenZen Clien
+  auto clientPair = make_client();
+  auto& clientError = clientPair.first;
+  client_ = clientPair.second;
+
+  if (clientError) {
+    AINFO << "Cannot create OpenZen client";
+    return clientError;
+  }
+
+  // connect to sensor on IO System by the sensor name
+  auto sensorPair = client_.obtainSensorByName("dev", device_conf_.device_id());
+  auto& obtainError = sensorPair.first;
+  auto& sensor = sensorPair.second;
+  if (obtainError) {
+    AINFO << "Cannot connect to sensor";
+    client_.close();
+    return obtainError;
+  }
+
+  // check that the sensor has an IMU component
+  auto imuPair = sensor.getAnyComponentOfType(g_zenSensorType_Imu);
+  auto& hasImu = imuPair.first;
+  // auto imu = imuPair.second;
+
+  if (!hasImu) {
+    AERROR << "Connected sensor has no IMU";
+    client_.close();
+    return ZenError_WrongSensorType;
+  }
+
+  return true;
+}
+
+bool LPMSDriverComponent::Action() {
+  auto event = client_.waitForNextEvent();
+  if (event.second.component.handle == imu.component().handle) {
+    std::cout << "> Acceleration: \t x = " << event.second.data.imuData.a[0]
+              << "\t y = " << event.second.data.imuData.a[1]
+              << "\t z = " << event.second.data.imuData.a[2] << std::endl;
+    std::cout << "> Gyro: \t\t x = " << event.second.data.imuData.g[0]
+              << "\t y = " << event.second.data.imuData.g[1]
+              << "\t z = " << event.second.data.imuData.g[2] << std::endl;
+
+    auto const& d = event.second.data.imuData;
+
+    apollo::drivers::Imu imu;
+
+    auto header = imu.mutable_header();
+    header->set_timestamp_sec(apollo::cyber::Time::Now().ToSecond());
+    header->set_frame_id("imu");
+
+    // Fill orientation quaternion
+    auto orientation = imu.mutable_orientation();
+    orientation->set_qw(d.q[0]);
+    orientation->set_qx(-d.q[1]);
+    orientation->set_qy(-d.q[2]);
+    orientation->set_qz(-d.q[3]);
+
+    // Fill angular velocity data
+    //   // - scale from deg/s to rad/s
+    auto angular_velocity = imu.mutable_angular_velocity();
+    angular_velocity->set_x(d.g[0] * cDegToRad);
+    angular_velocity->set_y(d.g[1] * cDegToRad);
+    angular_velocity->set_z(d.g[2] * cDegToRad);
+
+    // Fill linear acceleration data
+    const float rosConversion = -1.0 * (!param.useLpmsAccelerationConvention) +
+                                1.0 * param.useLpmsAccelerationConvention;
+
+    auto linear_acceleration = imu.mutable_linear_acceleration();
+    linear_acceleration->set_x(rosConversion * d.a[0] * cEarthG);
+    linear_acceleration->set_y(rosConversion * d.a[1] * cEarthG);
+    linear_acceleration->set_z(rosConversion * d.a[2] * cEarthG);
+
+    // Units are microTesla in the LPMS library, Tesla in ROS.
+    auto magnetic_field = imu.mutable_magnetic_field();
+    magnetic_field->set_x(d.b[0] * cMicroToTelsa);
+    magnetic_field->set_y(d.b[1] * cMicroToTelsa);
+    magnetic_field->set_z(d.b[2] * cMicroToTelsa);
+
+    // Publish the messages
+    imu_writer_->Write(imu);
+  }
+
+  return true;
+}
+
+}  // namespace imu
+}  // namespace drivers
+}  // namespace apollo
