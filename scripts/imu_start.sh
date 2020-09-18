10d5b4373fa02cbccae79b28f27d84aa617a79ab
sunzengpeng <2285438399@qq.com>
2020-09-18 19:05:00 +0800

    Update imu_start.sh

--- scripts/imu_start.sh
+++ scripts/imu_start.sh
@@ -4,12 +4,9 @@ cd "${DIR}/.."
 
 source "${DIR}/apollo_base.sh"
 
-# TODO(all): This is just an initial commit. Velodyne 64 and 16 share lots of
-# things. We need to select their processes precisely in 'pgrep', 'pkill' and
-# the monitor module.
 function start() {
     LOG="${APOLLO_ROOT_DIR}/data/log/imu.out"
-    CMD="cyberlaunch imu imu.launch"
+    CMD="cyber_launch start /apollo/modules/drivers/imu/imu.launch"
     NUM_PROCESSES="$(pgrep -c -f "imu")"
     if [ "${NUM_PROCESSES}" -eq 0 ]; then
         eval "nohup ${CMD} </dev/null >${LOG} 2>&1 &"
@@ -20,7 +17,6 @@ function stop() {
     pkill -SIGTERM -f imu.launch
 }
 
-# run command_name module_name
 function run() {
     case $1 in
         start)
@@ -36,3 +32,4 @@ function run() {
 }
 
 run "$1"
+
