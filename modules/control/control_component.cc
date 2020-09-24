0e12efbd604a956799e0551e497ef9297341a215
zongbao <mickeyouou@gmail.com>
2020-09-24 14:39:12 +0800

    navigation: module init

--- modules/control/control_component.cc
+++ modules/control/control_component.cc
@@ -177,11 +177,13 @@ bool ControlComponent::Proc() {
       // set control cmd
       // check estop, ture: brake=10,torque=1, write
       if (is_destination) {
-        cmd->set_parking_brake(true);
         cmd->set_brake(control_conf_.soft_estop_brake());
         cmd->set_torque(1);
         cmd->set_rear_wheel_target(rear_wheel_angle_value);
         cmd->set_front_wheel_target(front_wheel_angle_value);
+        if(chassis_.speed_mps() < 1e-6){
+          cmd->set_parking_brake(true);
+        }
       } else {
         drivemotor_torque = (drivemotor_torque < control_conf_.max_torque())
                                 ? drivemotor_torque
